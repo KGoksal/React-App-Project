3fb7402c73d648726d3f5f0caee422452ec7ea95
Kadir Goksal <136205319+KGoksal@users.noreply.github.com>
2024-09-10 04:15:00 -0500

    Create Fargate-Deployment.tf

---
+++ Fargate-Deployment.tf
@@ -0,0 +1,29 @@
+#Sample AWS ECS Fargate Deployment
+
+provider "aws" {
+  region = "us-west-2"
+}
+
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
+}
+
+resource "aws_security_group" "alb_sg" {
+  vpc_id = aws_vpc.main.id
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_lb" "app_alb" {
+  name               = "app-alb"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = [aws_security_group.alb_sg.id]
+  subnets            = [aws_subnet.main.id]
+}
+
+# ECS services, tasks, and ALB target groups must be defined
